970f0a695e45f1abc6fe0d86bc00a40a7ea5bf1c
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-05-17 03:52:36 +0700

    paticle emission

---
+++ ParticleEmission/ParticleEmission/ParticleEmissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParticleEmissionApp.swift
+//  ParticleEmission
+//
+//  Created by Adrian Suryo Abiyoga on 02/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ParticleEmissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
